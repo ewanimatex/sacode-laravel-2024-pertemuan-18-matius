b432fe8a2ef85424159a0744bf3e5e38f93fd1af
Matius ewani <ewanimatex@gmail.com>
2024-10-08 07:42:25 -0700

    sacode-laravel-2024-pertemuan-18-matius

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <h1>Welcome Billy</h1>
+    <div class="col-12">
+                <div class="table-reposinve">
+
+                    <table>
+                        <tr>
+                            <th>No</th>
+                            <th>NIM</th>
+                            <th>Nama</th>
+                            <th>Jurusan</th>
+                            <th>#</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>16411045</td>
+                            <td>Samuel</td>
+                            <td>Teknik Informatika</td>
+                            <td>
+                                <a href="">Detail</a>
+                                <a href="">Ubah</a>
+                                <a href="">Hapusss</a>
+
+
+                            </td>
+                        </tr>
+                    </table>
+
+
+                </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
